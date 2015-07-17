d40a6977403a1d1bb1dde8d0ee7449c0062ec485
Erik Lundgren <eriklundgren93@gmail.com>
2015-07-17 16:21:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,11 @@ echo "Installing dependencies..."
 sudo apt-get install libopus0 libexpat1 libssl1.0.0 libasound2 libudev0 libavahi-client3 libcurl3 libevdev2 libavahi-common3 libc6
 echo "Installing moonlight..."
 sudo dpkg -i ./moonlight-osmc.deb
-echo "Auto-loading snd-bcm2835..."
-sudo echo snd-bcm2835 >> /etc/modules
-sudo modprobe snd-bcm2835
 echo "Copying scripts..."
-sudo mkdir /opt/moonlight-osmc
-sudo mv ./scripts/* /opt/moonlight-osmc/
+sudo mkdir /opt/moonlight
+sudo mv ./scripts/* /opt/moonlight/
 echo "Setting permissions..."
-sudo chmod -R 755 /opt/moonlight-osmc/
+sudo chmod -R 755 /opt/moonlight/
 echo "Generating .moonlight.conf"
 mkdir ~/.moonlight
 echo "MOONLIGHT_OPTS = '-1080 -30fps'" > ~/.moonlight/moonlight.conf
