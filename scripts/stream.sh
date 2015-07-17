76e5561a8abc62bb85618a2dc78415ae6dc44b5b
Erik Lundgren <eriklundgren93@gmail.com>
2015-07-17 15:27:02 +0200

    Update stream.sh

--- scripts/stream.sh
+++ scripts/stream.sh
@@ -1,5 +1,5 @@
 # Originally written by miko
 # Modified by dodslaser
 . ~/.moonlight.conf
-(cd ~/.moonlight && sudo moonlight pair $GAMESTREAM_IP)
-(cd ~/.moonlight && sudo moonlight stream $GAMESTREAM_IP $MOONLIGHT_OPTS)
+sudo moonlight pair $GAMESTREAM_IP
+sudo moonlight stream $GAMESTREAM_IP $MOONLIGHT_OPTS
