8c9453465ae24345d4340c33851157758a812049
Erik Lundgren <eriklundgren93@gmail.com>
2015-07-17 16:22:19 +0200

    Update stream.sh

--- scripts/stream.sh
+++ scripts/stream.sh
@@ -1,5 +1,6 @@
 # Originally written by miko
 # Modified by dodslaser
 . ~/.moonlight.conf
+sudo modprobe snd-bcm2835
 sudo moonlight pair $GAMESTREAM_IP
 sudo moonlight stream $GAMESTREAM_IP $MOONLIGHT_OPTS
