5ea6d417a58905ad25cb8c1bee4e42743aa559fc
Erik Lundgren <eriklundgren93@gmail.com>
2015-07-17 15:24:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 # Originally written by dodslaser
+echo "Installing dependencies..."
+sudo echo 'deb http://archive.itimmer.nl/raspbian/moonlight wheezy main' >> /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install libopus0 libexpat1 libssl1.0.0 libasound2 libudev0 libavahi-client3 libcurl3 libevdev2 libavahi-common3 libc6
+echo "Installing moonlight..."
+sudo dpkg -i ./moonlight-osmc.deb
 echo "Auto-loading snd-bcm2835..."
-echo snd-bcm2835 > /etc/modules
+sudo echo snd-bcm2835 >> /etc/modules
 sudo modprobe snd-bcm2835
 echo "Copying scripts..."
-mkdir /opt/moonlight-osmc
+sudo mkdir /opt/moonlight-osmc
 sudo mv ./scripts/* /opt/moonlight-osmc/
 echo "Setting permissions..."
 sudo chmod -R 755 /opt/moonlight-osmc/
