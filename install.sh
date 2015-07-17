31c7f024ee7a546a1aca777e7686769c60fe74e7
Erik Lundgren <eriklundgren93@gmail.com>
2015-07-17 15:20:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,12 @@
 # Originally written by dodslaser
 echo "Auto-loading snd-bcm2835..."
 echo snd-bcm2835 > /etc/modules
-sh sudo modprobe snd-bcm2835
+sudo modprobe snd-bcm2835
 echo "Copying scripts..."
 mkdir /opt/moonlight-osmc
-mv ./scripts/moonlight.sh /opt/moonlight-osmc/moonlight.sh
-mv ./scripts/moonlight-watchdog.sh /opt/moonlight-osmc/moonlight-watchdog.sh
-mv ./scripts/stream.sh /opt/moonlight-osmc/stream.sh
+sudo mv ./scripts/* /opt/moonlight-osmc/
 echo "Setting permissions..."
-chmod 755 /opt/moonlight-osmc/moonlight.sh
-chmod 755 /opt/moonlight-osmc/moonlight-watchdog.sh
-chmod 755 /opt/moonlight-osmc/stream.sh
+sudo chmod -R 755 /opt/moonlight-osmc/
 echo "Generating .moonlight.conf"
 echo "MOONLIGHT_OPTS = '-1080 -30fps'" > ~/.moonlight/moonlight.conf
 echo "Enter the IP of the PC you will be streaming from:"
