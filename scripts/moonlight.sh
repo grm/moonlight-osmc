42d4ca7eb577da0f24838b7fee8292a2e2846815
Erik Lundgren <eriklundgren93@gmail.com>
2015-07-17 16:26:36 +0200

    Update moonlight.sh

--- scripts/moonlight.sh
+++ scripts/moonlight.sh
@@ -4,9 +4,9 @@
 sudo openvt -c 7 -s -f clear
 sudo openvt -c 7 -s -f echo "Running moonlight from KODI"
 # Start watchdog
-sudo su osmc -c "sh /opt/moonlight-osmc/moonlight-watchdog.sh &" &
+sudo su osmc -c "sh /opt/moonlight/moonlight-watchdog.sh &" &
 # Start stream
-sudo su osmc -c "nohup openvt -c 7 -s -f sh /opt/moonlight-osmc/stream.sh >/dev/null 2>&1 &" &
+sudo su osmc -c "nohup openvt -c 7 -s -f sh /opt/moonlight/stream.sh >/dev/null 2>&1 &" &
 sudo openvt -c 7 -s -f clear
 sleep 2
 # Stop mediacenter
