89d362f4481386444ad2e870e5598a7cfdfcb21b
Erik Lundgren <eriklundgren93@gmail.com>
2015-07-18 15:53:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,15 +7,15 @@ sudo dpkg -i ./moonlight-osmc.deb
 echo "Copying scripts..."
 sudo rm -rf /opt/moonlight
 sudo mkdir /opt/moonlight
-sudo mv ./scripts/* /opt/moonlight/
+sudo mv ./scripts/* /opt/moonlight-osmc/
 echo "Setting permissions..."
-sudo chmod -R 755 /opt/moonlight/
+sudo chmod -R 755 /opt/moonlight-osmc/
 echo "Generating .moonlight.conf"
-sudo rm -rf /home/osmc/.moonlight
-mkdir /home/osmc/.moonlight
-echo "#!/bin/sh" > /home/osmc/.moonlight/moonlight.conf
-echo "MOONLIGHT_OPTS='-1080 -30fps'" >> /home/osmc/.moonlight/moonlight.conf
+sudo rm -rf /home/osmc/.moonlight-osmc
+mkdir /home/osmc/.moonlight-osmc
+echo "#!/bin/sh" > /home/osmc/.moonlight-osmc/moonlight-osmc.conf
+echo "MOONLIGHT_OPTS='-1080 -30fps'" >> /home/osmc/.moonlight-osmc/moonlight-osmc.conf
 echo "Enter the IP of the PC you will be streaming from:"
 read GEN_GAMESTREAM_IP
-echo "GAMESTREAM_IP='$GEN_GAMESTREAM_IP'" >> /home/osmc/.moonlight/moonlight.conf
-chmod 755 ~/.moonlight/moonlight.conf
+echo "GAMESTREAM_IP='$GEN_GAMESTREAM_IP'" >> /home/osmc/.moonlight-osmc/moonlight-osmc.conf
+chmod 755 ~/.moonlight-osmc/moonlight-osmc.conf
